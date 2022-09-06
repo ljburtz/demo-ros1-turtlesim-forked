57115e2d7dda188b2b28f8bcee51c89ef1c45bbf
Louis Burtz <ljburtz@gmail.com>
2022-09-06 15:39:15 +0900

    remove turtlesim viz via another turtle -> use plotjuggler instead to be more generic and avoid turtlesim timing issues

--- ros_workspace/src/turtle_odometry/src/turtle_odom.py
+++ ros_workspace/src/turtle_odometry/src/turtle_odom.py
@@ -2,7 +2,6 @@
 import rospy
 from turtlesim.msg import Pose as TurtlePose
 import random
-from turtlesim.srv import TeleportAbsolute, SetPen, Spawn, Kill
 import numpy as np
 
 class TurtleOdom():
@@ -10,33 +9,13 @@ class TurtleOdom():
         self.name = name
         self.cb_throttle = rospy.Time.now()
 
-        # for odometry visualization, instantiate another turtle that will follow the odometry estimate
+        # initialize odometry estimate
         self.prev_pose = rospy.wait_for_message(
             f'/{self.name}/pose',
             TurtlePose,
             timeout=None
         )
-        self.name_odom = self.name + "_odom"
-        try:
-            rospy.wait_for_service(f"/kill")
-            self.srv_kill = rospy.ServiceProxy(f"/kill", Kill)
-            self.srv_kill(self.name_odom)
-            rospy.sleep(0.1)
-        except:  # catch the exception if turtle does not exist
-            pass
-        rospy.wait_for_service(f"/spawn")
-        self.srv_spawn = rospy.ServiceProxy(f"/spawn", Spawn)
-        self.srv_spawn(
-            x=self.prev_pose.x,
-            y=self.prev_pose.y,
-            theta=self.prev_pose.theta,
-            name=self.name_odom,
-        )
-        rospy.wait_for_service(f"/{self.name_odom}/teleport_absolute")
-        rospy.wait_for_service(f"/{self.name_odom}/set_pen")
-        self.srv_odom_pen = rospy.ServiceProxy(f"/{self.name_odom}/set_pen", SetPen)
-        self.srv_odom_teleport = rospy.ServiceProxy(f"/{self.name_odom}/teleport_absolute", TeleportAbsolute)
-        self.srv_odom_pen(r=0, g=0, b=0, width=5, off=False)
+        self.odometry = self.prev_pose
 
         # setup odometry publisher
         self.pub_odometry = rospy.Publisher(
@@ -44,8 +23,7 @@ class TurtleOdom():
             TurtlePose,
             queue_size=2
         )
-        self.odometry = self.prev_pose
-        # just for demo purposes
+        # for demo purposes: mock odometry
         # subscribe to pose ground truth and register a callback that will add noise and calculate the odometry
         self.sub_pose = rospy.Subscriber(
             f'/{self.name}/pose',
@@ -70,8 +48,8 @@ class TurtleOdom():
         delta_forward = np.sqrt(delta_x**2 + delta_y**2)
         self.prev_pose = odom
 
-        noise_factor_theta = 0.01
-        noise_factor_forward = 0.1
+        noise_factor_theta = 0.1
+        noise_factor_forward = 0.5
         noise_theta = noise_factor_theta * random.uniform(-1, 1)
         noise_forward = noise_factor_forward * random.uniform(-1, 1)
 
@@ -87,20 +65,13 @@ class TurtleOdom():
         # Finaly, publish the estimated odometry
         self.pub_odometry.publish(self.odometry)
 
-        # for visualization, teleport a turtle to the odometry estimate
-        self.srv_odom_teleport(
-            x=self.odometry.x,
-            y=self.odometry.y,
-            theta=self.odometry.theta,
-        )
-
-
 
 def main():
     rospy.init_node("turtle_odometry")
     try:
+        rospy.sleep(3)
         to = TurtleOdom("turtle1")
-        rospy.spin()  # needed?
+        rospy.spin()
     except rospy.ROSInterruptException:
         pass
 
